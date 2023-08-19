37266f4d219f1297bcc38f5510fe1ae3486c75a5
Jipson <jipson09saad@gmail.com>
2023-08-19 08:23:11 -0500

    feat: update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import helmet from 'helmet';
 // import rateLimit from 'express-rate-limit';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFastifyApplication } from '@nestjs/platform-fastify'
 
 const port = process.env.PORT_SERVER || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
@@ -21,14 +21,12 @@ async function bootstrap() {
       max: 100, // limit each IP to 100 requests per windowMs
     }),
   );*/
-  app.useStaticAssets(
-    join(__dirname, '..', 'uploads'),
-    {
-      prefix: '/uploads',
-      index: false,
-    },
-  );
-  
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'uploads'),
+    prefix: '/uploads',
+    index: false
+  });
+
   await app.listen(port);
 }
 bootstrap();
